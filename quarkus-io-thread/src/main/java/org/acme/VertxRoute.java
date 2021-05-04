1f0bd0052ae89cf0db6c6afe138a1c8e5312004d
Erik Simonsen <erik.simonsen@gmx.de>
2021-05-04 22:09:07 +0200

    Changed class, as request Obj is not used anymore

--- quarkus-io-thread/src/main/java/org/acme/VertxRoute.java
+++ quarkus-io-thread/src/main/java/org/acme/VertxRoute.java
@@ -27,8 +27,8 @@ public class VertxRoute {
 
 	@Route(path = "/hello/:name", methods = HttpMethod.GET)
 	void greetings(RoutingExchange ex) {
-		Set<ConstraintViolation<RequestObj>> violations = validator.validate(new RequestWrapper(ex.getParam("name").get()));
-		if( violations.size() == 0) {
+		Set<ConstraintViolation<RequestWrapper>> violations = validator.validate(new RequestWrapper(ex.getParam("name").get()));
+		if (violations.size() == 0) {
 			ex.ok(messageService.sayHello(ex.getParam("name").orElse("world")));
 		} else {
 			StringBuilder vaidationError = new StringBuilder();
